5c61144608fb361ace89ba1e6398b8c5577b45a5
jimenalezcano <82828483+jimenalezcano@users.noreply.github.com>
2021-04-19 12:49:25 -0300

    Add files via upload

---
+++ LezcanoClase1Complementario/complementario.js
@@ -0,0 +1,10 @@
+// Pido datos al usuario
+var nombre = prompt("Cual es tu nombre?")
+
+var apellido = prompt("Cual es tu apellido?")
+
+// Concateno los datos en una variable
+var concateno = nombre  + "  " + apellido
+
+//Muestro resultado por medio de un alert
+var resultado = alert("Bienvenido/a " + " " +  concateno + "!")
